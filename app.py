40f6ec222f2e8d35bf5b80b7a6d0170d322b5e42
kanchanproseth <kanchanproseth@users.noreply.github.com>
2017-11-06 11:03:31 +0700

    using bcrypt for encrypt password

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
-from flask import request
-from security import authenticate, identity
 from flask_pymongo import PyMongo
+from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -12,36 +11,37 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/API'
 app.secret_key = 'proseth123'
 api = Api(app)
 mongo = PyMongo(app)
-jwt = JWT(app, authenticate, identity)
+bcrypt = Bcrypt(app)
 
 
 class UserRegister(Resource):
-    @jwt_required()
     def post(self):
         username = request.json['username']
         password = request.json['password']
+        # print(pas)
         user = mongo.db.users
+        pw_hash = bcrypt.generate_password_hash(password)
         for u in user.find():
           if u['username'] == username:
             return {'message': 'this user name is existed'}, 404
-        user_id = user.insert({'username': username, 'password': password})
+        user_id = user.insert({'username': username, 'password': pw_hash})
         new_user = user.find_one({'_id': user_id})
-        output = {'username': new_user['username'],
-                  'password': new_user['password']}
-        return {'output': output}, 200
+        return {'message': 'Register Success'}, 200
 
 
 class USerSignIn(Resource):
-    @jwt_required()
     def post(self):
         username = request.json['username']
         password = request.json['password']
         user = mongo.db.users
+        pw_hash = bcrypt.generate_password_hash(password)
         if user.find_one({'username': username}):
-          if user.find_one({'password': password}):
-            return {'message': 'Log In successfully'}, 200
-          else:
-            return {'message': 'wrong password'}, 404
+        #   if user.find_one({'password': pw_hash}):
+            for u in user.find():
+                if bcrypt.check_password_hash(u['password'], password):
+                    return {'message': 'Log In successfully'}, 200
+                else:
+                    return {'message': 'wrong password'}, 404
         return {'message': 'please check your username again'}
 
 
