7cf2ffefb4975be5b3efec9cf497f6f69042df49
kanchanproseth <kanchanproseth@users.noreply.github.com>
2017-11-06 16:50:05 +0700

    remove unuse import

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
 from flask_pymongo import PyMongo
 from flask.ext.bcrypt import Bcrypt
 
